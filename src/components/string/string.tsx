fc3b4e4b47a37828f40997cabc55f43c365b6da3
Andrei Kolosov <oldliberty@yande.ru>
2022-10-05 18:43:37 +0300

    feat: string solution murkup

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import styles from './string.module.css';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
+import { Input } from '../ui/input/input';
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const StringComponent: React.FC = () => {
+
+
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form}>
+        <Input isLimitText />
+        <Button text="Развернуть" />
+      </form>
+      <ul className={styles.list}>
+        <li>
+          <Circle />
+        </li>
+        <li>
+          <Circle />
+        </li>
+        <li>
+          <Circle />
+        </li>
+        <li>
+          <Circle />
+        </li>
+      </ul>
     </SolutionLayout>
   );
 };
