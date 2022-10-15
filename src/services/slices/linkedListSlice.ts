7dc0d6c10dfe27ba9b358592e9da77a9f6819b38
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-15 15:22:42 +0200

    feat: create handle remove from head & tail

--- src/services/slices/linkedListSlice.ts
+++ src/services/slices/linkedListSlice.ts
@@ -1,8 +1,8 @@
 import { ElementStates } from './../../types/element-states';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IInitialState } from '../../types/linkedList.types';
-// Вынести в Utils
-const createListItem = (currentState: IInitialState, value: string) => ({
+// Вынести в Utils----------------
+const createInitialListItem = (currentState: IInitialState, value: string) => ({
   value,
   index: currentState.listTail,
   head: currentState.listHead === currentState.listTail ? 'head' : '',
@@ -11,7 +11,7 @@ const createListItem = (currentState: IInitialState, value: string) => ({
   extraClass: true,
   extraClassModifier: ElementStates.Default,
 });
-
+// -----------------------------------
 const initialState: IInitialState = {
   linkedList: [],
   listHead: 0,
@@ -27,7 +27,7 @@ export const linkedListSlice = createSlice({
     initList(store, action: PayloadAction<string[]>) {
       const initialValues = action.payload;
       initialValues.forEach((value) => {
-        const item = createListItem(store, value)
+        const item = createInitialListItem(store, value);
         store.linkedList.push(item);
         if (store.listHead !== store.listTail) {
           store.linkedList[store.listTail - 1].tail = false;
@@ -36,8 +36,27 @@ export const linkedListSlice = createSlice({
         store.listLength += 1;
       });
       store.linkedList[store.listTail - 1].extraClass = false;
-    }
+    },
+    removeFromTail(store) {
+      if (store.listLength === 0) return;
+      store.linkedList.pop();
+      store.listTail -= 1;
+      store.listLength -= 1;
+      if (store.listHead !== store.listTail) {
+        store.linkedList[store.listTail - 1].tail = true;
+        store.linkedList[store.listTail - 1].extraClass = false;
+      }
+    },
+    removeFromHead(store) {
+      if (store.listLength === 0) return;
+      store.linkedList.shift();
+      store.linkedList.map((item) => item.index -= 1);
+      if (store.linkedList[store.listHead] !== undefined) {
+        store.linkedList[store.listHead].head = 'head';
+      }
+      store.listLength -= 1;
+    },
   },
 });
 
-export const { initList } = linkedListSlice.actions;
+export const { initList, removeFromTail, removeFromHead } = linkedListSlice.actions;
