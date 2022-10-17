7aa76fb23cd98c814c4825021a3b66da88d9f8e0
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-17 23:31:11 +0200

    feat: stack solution

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -4,34 +4,60 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from '../ui/input/input';
 import { Button } from '../ui/button/button';
 import { Circle } from '../ui/circle/circle';
-import { useAppDispatch, useAppSelector } from '../../services/hooks';
-import { clear } from '../../services/slices/stackSlice';
-import { ADD_TO_STACK, REMOVE_FROM_STACK } from '../../services/sagas/actions/stackActions';
+import { Stack } from '../../utils/stack';
+import { ElementStates } from '../../types/element-states';
+import { delay } from '../../utils/utils';
+import { SHORT_DELAY_IN_MS } from '../../constants/delays';
+
+export type TStackElement = {
+  value: string;
+  state: ElementStates;
+};
+
+export type TProcess = { isAdding: boolean, isRemoving: boolean}
 
 export const StackPage: React.FC = () => {
-const [value, setValue] = useState<string>('');
-const maxInputValueLength = 4;
-const { inProcess, stack, peak } = useAppSelector((store) => store.stack);
-const dispatch = useAppDispatch();
+  const stackInstance = new Stack<TStackElement>();
+  const [value, setValue] = useState<string>('');
+  const [stack, setStack] = useState<Stack<TStackElement>>(stackInstance);
+  const [inProcess, setInProcess] = useState<TProcess>({ isAdding: false, isRemoving: false});
+  const maxInputValueLength = 4;
 
-const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-  setValue(e.target?.value);
-}
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target?.value as string);
+  }
 
-const handleAdd = () => {
-  dispatch({ type: ADD_TO_STACK, value })
-  setValue('');
-}
+  const handleAdd = async() => {
+    setInProcess((state) => ({...state, isAdding: true}));
+    const inputValue = value;
+    const stackTemp = stack;
+    stackTemp.push({ value: inputValue, state: ElementStates.Changing });
+    setValue('');
+    setStack(stackTemp);
+    await delay(SHORT_DELAY_IN_MS);
+    const stackPeak = stackTemp.peak();
+    if(stackPeak) stackPeak.state = ElementStates.Default;
+    setInProcess((state) => ({ ...state, isAdding: false }));
+  }
 
-const handleRemove = () => {
-  dispatch({ type: REMOVE_FROM_STACK })
-  setValue('');
-}
+  const handleRemove = async() => {
+    setInProcess((state) => ({ ...state, isRemoving: true }));
+    const stackTemp = stack;
+    const stackPeak = stackTemp.peak();
+    if (stackPeak) stackPeak.state = ElementStates.Changing;
+    await delay(SHORT_DELAY_IN_MS);
+    stackTemp.pop();
+    setStack(stackTemp);
+    setInProcess((state) => ({ ...state, isRemoving: false }));
+  }
 
-const handleClear = () => {
-  dispatch(clear());
-  setValue('');
-}
+  const handleClear = async() => {
+    setInProcess((state) => ({ ...state, isRemoving: true }));
+    const stackTemp = stack;
+    stackTemp.clear();
+    setStack(stackTemp);
+    setInProcess((state) => ({ ...state, isRemoving: false }));
+  }
 
   return (
     <SolutionLayout title="Стек">
@@ -42,20 +68,38 @@ const handleClear = () => {
           value={value}
           onChange={handleInputChange}
           placeholder="Введите текст"
-          disabled={stack.length >= 6}
+          disabled={stack.getSize() >= 6}
         />
-        <Button type="button" text="Добавить" onClick={handleAdd} disabled={inProcess || stack.length >= 6} />
-        <Button type="button" text="Удалить" onClick={handleRemove} disabled={inProcess} extraClass={styles.controls__removeBtn}/>
-        <Button type="button" text="Очистить" onClick={handleClear} disabled={inProcess} />
+        <Button
+          type="button"
+          text="Добавить"
+          onClick={handleAdd}
+          isLoader={inProcess.isAdding}
+          disabled={inProcess.isRemoving || stack.getSize() >= 6 || !value}
+        />
+        <Button
+          type="button"
+          text="Удалить"
+          onClick={handleRemove}
+          isLoader={inProcess.isRemoving}
+          disabled={inProcess.isAdding}
+          extraClass={styles.controls__removeBtn}
+        />
+        <Button
+          type="button"
+          text="Очистить"
+          onClick={handleClear}
+          disabled={inProcess.isAdding || inProcess.isRemoving}
+        />
       </div>
       <ul className={styles.list}>
-        {stack.map((item, index) => (
+        {stack.getElements().map((item, index) => (
           <li key={index}>
             <Circle
               state={item.state}
-              letter={item.value}
+              letter={item.value || ''}
               index={index}
-              head={stack.length - 1 === index && item.value === peak?.value ? 'top' : ''}
+              head={stack.getSize() - 1 === index  ? 'top' : ''}
             />
           </li>
         ))}
